f02f6b985b46fd8a4a9ca4f2fd1ac229293884fd
Jrm Treiber <72030730+jrmtreiber@users.noreply.github.com>
2025-02-21 20:58:52 -0300

    ATT 4.

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,30 @@ function moveSlide(step) {
     if (index < 0) index = totalImages - 1;
     images.style.transform = `translateX(-${index * 100}%)`;
 }
+
+// 
+
+window.onscroll = function() {
+    stickyHeader();
+  };
+  
+  var header2 = document.getElementById("header2");
+  
+  function stickyHeader() {
+    // Verifica se a largura da tela é maior que 768px (desktop)
+    if (window.innerWidth > 768) {
+      if (window.pageYOffset > 100) {
+        header2.style.position = "fixed";
+        header2.style.top = "0";
+      } else {
+        header2.style.position = "absolute"; // Retorna ao comportamento normal
+        header2.style.top = "auto";
+      }
+    } else {
+      // Se for mobile, não faz nada (pode também garantir que o header não seja alterado)
+      header2.style.position = "absolute";
+      header2.style.top = "auto";
+    }
+  }
+  
+  
