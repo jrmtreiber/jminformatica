b3c22b64ebd0211d7b231f35fbf61d79e43125b7
Jrm Treiber <72030730+jrmtreiber@users.noreply.github.com>
2025-02-21 22:35:01 -0300

    ATT 7.

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,48 @@
-let index = 0;
-function moveSlide(step) {
-    const images = document.querySelector('.carousel-images');
-    const totalImages = document.querySelectorAll('.carousel-images img').length;
-    index += step;
-    if (index >= totalImages) index = 0;
-    if (index < 0) index = totalImages - 1;
-    images.style.transform = `translateX(-${index * 100}%)`;
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const newTransform = `translateX(-${currentIndex * 100}%)`;
+    document.querySelector('.carousel-images').style.transform = newTransform;
 }
+
+function moveSlide(direction) {
+    showSlide(currentIndex + direction);
+}
+
+setInterval(() => {
+    showSlide(currentIndex + 1);
+}, 10000);
+
+showSlide(currentIndex);
 
 // 
 
-window.onscroll = function() {
+window.onscroll = function () {
     stickyHeader();
-  };
-  
-  var header2 = document.getElementById("header2");
-  
-  function stickyHeader() {
+};
+
+var header2 = document.getElementById("header2");
+
+function stickyHeader() {
     if (window.innerWidth > 768) {
-      if (window.pageYOffset > 100) {
-        header2.style.position = "fixed";
-        header2.style.top = "0";
-      } else {
-        header2.style.position = "absolute"; 
-        header2.style.top = "auto";
-      }
+        if (window.pageYOffset > 100) {
+            header2.style.position = "fixed";
+            header2.style.top = "0";
+        } else {
+            header2.style.position = "absolute";
+            header2.style.top = "auto";
+        }
     } else {
-      header2.style.position = "absolute";
-      header2.style.top = "auto";
+        header2.style.position = "absolute";
+        header2.style.top = "auto";
     }
-  }
-  
-  
+}
+
