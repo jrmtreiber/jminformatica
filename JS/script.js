6fd97920b4e3cb2fad1d21ae1f6b3ae3d9dd07af
Jrm Treiber <72030730+jrmtreiber@users.noreply.github.com>
2025-02-21 21:19:15 -0300

    ATT 5.

--- JS/script.js
+++ JS/script.js
@@ -17,17 +17,15 @@ window.onscroll = function() {
   var header2 = document.getElementById("header2");
   
   function stickyHeader() {
-    // Verifica se a largura da tela é maior que 768px (desktop)
     if (window.innerWidth > 768) {
       if (window.pageYOffset > 100) {
         header2.style.position = "fixed";
         header2.style.top = "0";
       } else {
-        header2.style.position = "absolute"; // Retorna ao comportamento normal
+        header2.style.position = "absolute"; 
         header2.style.top = "auto";
       }
     } else {
-      // Se for mobile, não faz nada (pode também garantir que o header não seja alterado)
       header2.style.position = "absolute";
       header2.style.top = "auto";
     }
